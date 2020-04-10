4ea3aa80b29677eeee95e6e5ae2d21e6c4c5c1d4
Nick Colgan <nick.colgan@gmail.com>
2020-04-10 01:13:13 -0500

    Persistent session

--- lib/pow_assent/phoenix/controllers/authorization_controller.ex
+++ lib/pow_assent/phoenix/controllers/authorization_controller.ex
@@ -9,13 +9,13 @@ defmodule PowAssent.Phoenix.AuthorizationController do
   alias PowEmailConfirmation.Phoenix.ControllerCallbacks, as: EmailConfirmationCallbacks
   alias PowPersistentSession.Plug, as: PowPersistentSessionPlug
 
-  plug :require_authenticated when action in [:delete]
-  plug :assign_callback_url when action in [:new, :callback]
-  plug :init_session when action in [:new, :callback]
-  plug :assign_request_path when action in [:callback]
-  plug :load_session_params when action in [:callback]
-  plug :set_registration_option when action in [:callback]
-  plug :load_user_by_invitation_token when action in [:callback]
+  plug(:require_authenticated when action in [:delete])
+  plug(:assign_callback_url when action in [:new, :callback])
+  plug(:init_session when action in [:new, :callback])
+  plug(:assign_request_path when action in [:callback])
+  plug(:load_session_params when action in [:callback])
+  plug(:set_registration_option when action in [:callback])
+  plug(:load_user_by_invitation_token when action in [:callback])
 
   @spec process_new(Conn.t(), map()) :: {:ok, binary(), Conn.t()} | {:error, any(), Conn.t()}
   def process_new(conn, %{"provider" => provider}) do
@@ -31,31 +31,41 @@ defmodule PowAssent.Phoenix.AuthorizationController do
     |> maybe_store_persistent_session_value()
     |> redirect(external: url)
   end
+
   def respond_new({:error, error, _conn}), do: handle_strategy_error(error)
 
   defp maybe_store_session_params(%{private: %{pow_assent_session_params: params}} = conn),
     do: Plug.put_session(conn, :session_params, params)
+
   defp maybe_store_session_params(conn), do: conn
 
   defp maybe_store_request_path(%{params: %{"request_path" => request_path}} = conn),
     do: Plug.put_session(conn, :request_path, request_path)
+
   defp maybe_store_request_path(conn), do: conn
 
   defp maybe_store_invitation_token(%{params: %{"invitation_token" => token}} = conn),
     do: Plug.put_session(conn, :invitation_token, token)
+
   defp maybe_store_invitation_token(conn), do: conn
 
-  defp maybe_store_persistent_session_value(%{params: %{"persistent_session" => "true"}} = conn),
+  defp maybe_store_persistent_session_value(conn),
     do: Plug.put_session(conn, :store_persistent_session?, true)
-  defp maybe_store_persistent_session_value(conn), do: conn
 
-  @spec process_callback(Conn.t(), map()) :: {:ok, Conn.t()} | {:error, Conn.t()} | {:error, {atom(), map()} | map(), Conn.t()}
+  @spec process_callback(Conn.t(), map()) ::
+          {:ok, Conn.t()} | {:error, Conn.t()} | {:error, {atom(), map()} | map(), Conn.t()}
   def process_callback(conn, %{"provider" => provider} = params) do
     Plug.callback_upsert(conn, provider, params, conn.assigns.callback_url)
   end
 
-  @spec respond_callback({:ok, Conn.t()} | {:error, Conn.t()} | {:error, {atom(), map()} | map(), Conn.t()}) :: Conn.t()
-  def respond_callback({:ok, %{private: %{pow_assent_callback_state: {:ok, :create_user}}} = conn}) do
+  @spec respond_callback(
+          {:ok, Conn.t()}
+          | {:error, Conn.t()}
+          | {:error, {atom(), map()} | map(), Conn.t()}
+        ) :: Conn.t()
+  def respond_callback(
+        {:ok, %{private: %{pow_assent_callback_state: {:ok, :create_user}}} = conn}
+      ) do
     trigger_registration_email_confirmation_controller_callback(conn, fn conn ->
       conn
       |> maybe_create_persistent_session()
@@ -63,6 +73,7 @@ defmodule PowAssent.Phoenix.AuthorizationController do
       |> redirect(to: routes(conn).after_registration_path(conn))
     end)
   end
+
   def respond_callback({:ok, conn}) do
     trigger_session_email_confirmation_controller_callback(conn, fn conn ->
       conn
@@ -71,24 +82,44 @@ defmodule PowAssent.Phoenix.AuthorizationController do
       |> redirect(to: routes(conn).after_sign_in_path(conn))
     end)
   end
-  def respond_callback({:error, %{private: %{pow_assent_callback_state: {:error, :strategy}, pow_assent_callback_error: error}}}),
-    do: handle_strategy_error(error)
-  def respond_callback({:error, %{private: %{pow_assent_callback_error: {:bound_to_different_user, _changeset}}} = conn}) do
+
+  def respond_callback(
+        {:error,
+         %{
+           private: %{
+             pow_assent_callback_state: {:error, :strategy},
+             pow_assent_callback_error: error
+           }
+         }}
+      ),
+      do: handle_strategy_error(error)
+
+  def respond_callback(
+        {:error,
+         %{private: %{pow_assent_callback_error: {:bound_to_different_user, _changeset}}} = conn}
+      ) do
     conn
     |> put_flash(:error, extension_messages(conn).account_already_bound_to_other_user(conn))
     |> redirect(to: routes(conn).session_path(conn, :new))
   end
-  def respond_callback({:error, %{private: %{pow_assent_callback_error: {:invalid_user_id_field, changeset}}} = conn}) do
+
+  def respond_callback(
+        {:error,
+         %{private: %{pow_assent_callback_error: {:invalid_user_id_field, changeset}}} = conn}
+      ) do
     trigger_registration_email_confirmation_controller_callback(conn, fn conn ->
-      params   = Map.fetch!(conn.private, :pow_assent_callback_params)
+      params = Map.fetch!(conn.private, :pow_assent_callback_params)
       provider = Map.fetch!(conn.params, "provider")
 
       conn
       |> Plug.put_session(:callback_params, %{provider => params})
       |> Plug.put_session(:changeset, changeset)
-      |> redirect(to: routes(conn).path_for(conn, RegistrationController, :add_user_id, [provider]))
+      |> redirect(
+        to: routes(conn).path_for(conn, RegistrationController, :add_user_id, [provider])
+      )
     end)
   end
+
   def respond_callback({:error, conn}) do
     conn
     |> put_flash(:error, extension_messages(conn).could_not_sign_in(conn))
@@ -96,7 +127,7 @@ defmodule PowAssent.Phoenix.AuthorizationController do
   end
 
   defp trigger_registration_email_confirmation_controller_callback(conn, callback) do
-    config        = PowPlug.fetch_config(conn)
+    config = PowPlug.fetch_config(conn)
     %{user: user} = conn.private[:pow_assent_callback_params]
 
     cond do
@@ -105,11 +136,15 @@ defmodule PowAssent.Phoenix.AuthorizationController do
 
       extension_enabled?(config, PowEmailConfirmation) ->
         Pow.Phoenix.RegistrationController
-        |> EmailConfirmationCallbacks.before_respond(:create, to_email_confirmation_res(conn), config)
+        |> EmailConfirmationCallbacks.before_respond(
+          :create,
+          to_email_confirmation_res(conn),
+          config
+        )
         |> case do
-          {:ok, _user, conn}         -> callback.(conn)
+          {:ok, _user, conn} -> callback.(conn)
           {:error, _changeset, conn} -> callback.(conn)
-          {:halt, conn}              -> conn
+          {:halt, conn} -> conn
         end
 
       true ->
@@ -117,9 +152,17 @@ defmodule PowAssent.Phoenix.AuthorizationController do
     end
   end
 
-  defp to_email_confirmation_res(%{private: %{pow_assent_callback_state: {:error, _method}, pow_assent_callback_error: {_type, changeset}}} = conn) do
+  defp to_email_confirmation_res(
+         %{
+           private: %{
+             pow_assent_callback_state: {:error, _method},
+             pow_assent_callback_error: {_type, changeset}
+           }
+         } = conn
+       ) do
     {:error, changeset, conn}
   end
+
   defp to_email_confirmation_res(%{private: %{pow_assent_callback_state: {:ok, _method}}} = conn) do
     {:ok, PowPlug.current_user(conn), conn}
   end
@@ -140,12 +183,12 @@ defmodule PowAssent.Phoenix.AuthorizationController do
     Pow.Phoenix.SessionController
     |> EmailConfirmationCallbacks.before_respond(:create, {:ok, conn}, config)
     |> case do
-      {:ok, conn}   -> callback.(conn)
+      {:ok, conn} -> callback.(conn)
       {:halt, conn} -> conn
     end
   end
 
-  defp maybe_create_persistent_session(%{private: %{pow_assent_session: %{store_persistent_session?: true}}} = conn) do
+  defp maybe_create_persistent_session(conn) do
     user = PowPlug.current_user(conn)
 
     conn
@@ -156,7 +199,6 @@ defmodule PowAssent.Phoenix.AuthorizationController do
       _any -> conn
     end
   end
-  defp maybe_create_persistent_session(conn), do: conn
 
   @spec process_delete(Conn.t(), map()) :: {:ok, map(), Conn.t()} | {:error, any(), Conn.t()}
   def process_delete(conn, %{"provider" => provider}) do
@@ -169,38 +211,54 @@ defmodule PowAssent.Phoenix.AuthorizationController do
     |> put_flash(:info, extension_messages(conn).authentication_has_been_removed(conn))
     |> redirect(to: after_delete_path(conn))
   end
+
   def respond_delete({:error, {:no_password, _changeset}, conn}) do
     conn
-    |> put_flash(:error, extension_messages(conn).identity_cannot_be_removed_missing_user_password(conn))
+    |> put_flash(
+      :error,
+      extension_messages(conn).identity_cannot_be_removed_missing_user_password(conn)
+    )
     |> redirect(to: after_delete_path(conn))
   end
 
   defp after_delete_path(conn), do: routes(conn).registration_path(conn, :edit)
 
   defp assign_callback_url(conn, _opts) do
-    url = routes(conn).url_for(conn, AuthorizationController, :callback, [conn.params["provider"]])
+    url =
+      routes(conn).url_for(conn, AuthorizationController, :callback, [conn.params["provider"]])
 
     assign(conn, :callback_url, url)
   end
 
   defp init_session(conn, _opts), do: Plug.init_session(conn)
 
-  defp assign_request_path(%{private: %{pow_assent_session: %{request_path: request_path}}} = conn, _opts) do
+  defp assign_request_path(
+         %{private: %{pow_assent_session: %{request_path: request_path}}} = conn,
+         _opts
+       ) do
     conn
     |> Plug.delete_session(:request_path)
     |> Conn.assign(:request_path, request_path)
   end
+
   defp assign_request_path(conn, _opts), do: conn
 
-  defp load_session_params(%{private: %{pow_assent_session: %{session_params: params}}} = conn, _opts) do
+  defp load_session_params(
+         %{private: %{pow_assent_session: %{session_params: params}}} = conn,
+         _opts
+       ) do
     conn
     |> Conn.put_private(:pow_assent_session_params, params)
     |> Plug.delete_session(:session_params)
   end
+
   defp load_session_params(conn, _opts), do: conn
 
-  defp set_registration_option(%{private: %{pow_assent_registration: _any}} = conn, _opts), do: conn
-  defp set_registration_option(conn, _opts), do: Conn.put_private(conn, :pow_assent_registration, registration_path?(conn))
+  defp set_registration_option(%{private: %{pow_assent_registration: _any}} = conn, _opts),
+    do: conn
+
+  defp set_registration_option(conn, _opts),
+    do: Conn.put_private(conn, :pow_assent_registration, registration_path?(conn))
 
   defp registration_path?(conn) do
     [conn.private.phoenix_router, Helpers]
@@ -208,7 +266,10 @@ defmodule PowAssent.Phoenix.AuthorizationController do
     |> function_exported?(:pow_assent_registration_path, 3)
   end
 
-  defp load_user_by_invitation_token(%{private: %{pow_assent_session: %{invitation_token: token}}} = conn, _opts) do
+  defp load_user_by_invitation_token(
+         %{private: %{pow_assent_session: %{invitation_token: token}}} = conn,
+         _opts
+       ) do
     conn = Plug.delete_session(conn, :invitation_token)
 
     conn
@@ -223,7 +284,8 @@ defmodule PowAssent.Phoenix.AuthorizationController do
         PowPlug.assign_current_user(conn, user, config)
     end
   end
+
   defp load_user_by_invitation_token(conn, _opts), do: conn
 
-  defp handle_strategy_error(error), do: raise error
+  defp handle_strategy_error(error), do: raise(error)
 end
